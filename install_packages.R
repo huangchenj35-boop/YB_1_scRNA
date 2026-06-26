d53f617b6e881e39b1479e3f0bbf4355eca3e0ed
Chenjun Huang <huangchenj35@gmail.com>
2026-06-26 23:06:33 +0800

    Reset repository before Code Ocean upload

--- install_packages.R
+++
@@ -1,9 +0,0 @@
-## Deprecated for Code Ocean
-##
-## Package installation is intentionally not performed by this file.
-## Use packages.R to check package availability and run_codeocean.R as the
-## reviewer-safe entry point.
-
-message("install_packages.R is deprecated for the Code Ocean workflow.")
-message("Run: Rscript packages.R")
-message("Then run: Rscript run_codeocean.R")
